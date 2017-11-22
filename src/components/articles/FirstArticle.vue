121dd31bedcebe2eca06132784540919bbe2adbf
Durban-Designer <royce.birnbaum@gmail.com>
2017-11-22 10:51:58 -0700

    Basic Article

--- src/components/articles/FirstArticle.vue
+++ src/components/articles/FirstArticle.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="main">
-    <div>article</div>
+    <h1>QWC-VENUMM The Basics</h1>
+    <p>The Quick Venom stack refers to an application architecture based around nine core technologies;</p>
+    <ul>
+      <li>Quasar</li>
+      <li>Webpack</li>
+      <li>Cordova</li>
+      <li>Vue.js</li>
+      <li>Express</li>
+      <li>Node.js</li>
+      <li>Ubuntu Server</li>
+      <li>MongoDb</li>
+      <li>Mongoose</li>
+    </ul>
+    <p>
+      Each of these will be described in more detail below but before we dive into the specific technologies I want to talk a little bit about the amazing
+      functionality that is granted with this application stack. Firstly you can create rock solid and lightweight Single Page Applications easily and quickly thanks to
+      templating in both Quasar and Vue. Secondarily thanks to Quasar you can wrap your web application with Cordova to create hybrid web applications that run on both
+      IOS and Android. You can also wrap for electron and deploy to desktop computers allowing a true write once run anywhere mentality from one code base with just a
+      few simple tweaks. Add in the power of Node.js to provide package managment and you have a simple to learn quality architecture that will allow developers to pool
+      there efforts more effectively to produce quality performant software solutions to todays rapidly increasing marketplace. With all these promises you're probably thinking
+      "Durban it just cant be so! There is one team for every platform, that must have lag, its gonna be complicated, etc.." while fear not padiwan for once I show you the ease
+      of Quick Venom you wont be able to stop coding.
+    </p>
   </div>
 </template>
 
@@ -15,4 +37,8 @@
 </script>
 
 <style lang="less">
+  .main {
+    font-size: 1em;
+    text-align: justify;
+  }
 </style>
