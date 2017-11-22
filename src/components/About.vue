0887570d6e91d25c2f7360671208236c6fde9ee3
Durban-Designer <royce.birnbaum@gmail.com>
2017-11-22 11:05:08 -0700

    Tweaks

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="main">
-    <div>about</div>
+    <h1>QWC-VENUMM (Quick Venom)</h1>
+    <p>The Quick Venom stack refers to an application architecture based around nine core technologies;</p>
+    <ul>
+      <li>Quasar</li>
+      <li>Webpack</li>
+      <li>Cordova</li>
+      <li>Vue.js</li>
+      <li>Express</li>
+      <li>Node.js</li>
+      <li>Ubuntu Server</li>
+      <li>MongoDb</li>
+      <li>Mongoose</li>
+    </ul>
+    <p>
+      
+    </p>
   </div>
 </template>
 
