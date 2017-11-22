c90c333767697ade6aa6a3d145fe9c25539faf60
Durban-Designer <royce.birnbaum@gmail.com>
2017-11-22 11:53:38 -0700

    Added Dark Green

--- src/components/elements/Navbar.vue
+++ src/components/elements/Navbar.vue
@@ -146,7 +146,7 @@
     width: 80px;
     height: 80px;
     transform: scale(.9,.9);
-    background: url("../../assets/navbuttonAnimationGreenThin.svg");
+    background: url("../../assets/navbuttonAnimationDarkGreenThin.svg");
     background-repeat: no-repeat;
   }
 
