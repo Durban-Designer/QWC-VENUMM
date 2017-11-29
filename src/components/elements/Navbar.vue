0485ecc32cb045ef7f2abd87c37a258c51a4876b
Spiralis-Solutions <krisstjohn96@gmail.com>
2017-11-29 15:00:09 -0700

    Navbar Work

--- src/components/elements/Navbar.vue
+++ src/components/elements/Navbar.vue
@@ -3,8 +3,21 @@
     <div class="logo"></div>
     <div  v-on:click="homeNav" class="Home">HOME</div>
     <div  v-on:click="aboutNav" class="about">ABOUT</div>
-    <div  v-on:click="articlesNav" class="articles">ARTICLES</div>
+    <div  v-on:click="articlesToggle" class="articles">ARTICLES</div>
     <div class="sexyLine"></div>
+    <div v-bind:class="navpaneLogic">
+      <div v-on:click="quasarNav" class="quasar">Quasar</div>
+      <div v-on:click="webpackNav" class="webpack">Webpack</div>
+      <div v-on:click="cordovaNav" class="cordova">Cordova</div>
+      <div v-on:click="vueNav" class="vue">Vue.js</div>
+      <div v-on:click="expressNav" class="express">Express</div>
+      <div v-on:click="nodeNav" class="node">Node.js</div>
+      <div v-on:click="ubuntuNav" class="ubuntu">Ubuntu</div>
+      <div v-on:click="ubuntuHandsNav" class="ubuntuhands">Ubuntu Hands-On</div>
+      <div v-on:click="mongodbNav" class="mongodb">MongoDB</div>
+      <div v-on:click="mongooseNav" class="mongoose">Mongoose</div>
+      <div v-on:click="buildingNav" class="building">Building</div>
+    </div>
   </div>
 </template>
 
@@ -13,45 +26,15 @@
     name: 'Navbar',
     data: function () {
       return {
-        isToggled: false,
-        first: true
+        articleToggle: false
       }
     },
     props: ['loggedNav'],
     computed: {
-      navbutton: function () {
+      navpaneLogic: function () {
         return {
-          navButtonAnimation: this.isToggled,
-          navButtonAnimationExit: !this.isToggled && !this.first
-        }
-      },
-      navpane: function () {
-        return {
-          navpaneAnimation: this.isToggled,
-          navpaneAnimationExit: !this.isToggled && !this.first
-        }
-      },
-      about: function () {
-        return {
-          navtileAnimation: this.isToggled,
-          navtileAnimationExit: !this.isToggled && !this.first,
-          navtile: true
-        }
-      },
-      login: function () {
-        return {
-          hidden: this.loggedNav,
-          navtileAnimation: this.isToggled,
-          navtileAnimationExit: !this.isToggled && !this.first,
-          navtile: !this.loggedNav
-        }
-      },
-      logged: function () {
-        return {
-          hidden: !this.loggedNav,
-          navtileAnimation: this.isToggled,
-          navtileAnimationExit: !this.isToggled && !this.first,
-          navtile: this.loggedNav
+          hidden: !this.articleToggle,
+          navpane: this.articleToggle
         }
       }
     },
@@ -59,8 +42,48 @@
       homeNav: function () {
         this.$router.push('/')
       },
+      articlesToggle: function () {
+        if (this.articleToggle === false) {
+          this.articleToggle = true
+        } else if (this.articleToggle === true) {
+          this.articleToggle = false
+        }
+      },
       aboutNav: function () {
         this.$router.push('/about')
+      },
+      quasarNav: function () {
+        this.$router.push('/quasar')
+      },
+      webpackNav: function () {
+        this.$router.push('/webpack')
+      },
+      cordovaNav: function () {
+        this.$router.push('/cordova')
+      },
+      vueNav: function () {
+        this.$router.push('/vue')
+      },
+      expressNav: function () {
+        this.$router.push('/express')
+      },
+      nodeNav: function () {
+        this.$router.push('/node')
+      },
+      ubuntuNav: function () {
+        this.$router.push('/ubuntu')
+      },
+      ubuntuHandsNav: function () {
+        this.$router.push('/ubuntuhands')
+      },
+      mongodbNav: function () {
+        this.$router.push('/mongodb')
+      },
+      mongooseNav: function () {
+        this.$router.push('/mongoose')
+      },
+      buildingNav: function () {
+        this.$router.push('/building')
       },
       articlesNav: function () {
         this.$router.push('/firstarticle')
@@ -126,5 +149,9 @@
     background-color: #1bad4a;
     grid-row: 2;
     grid-column: 3;
+  }
+
+  .hidden {
+    display: none;
   }
 </style>
