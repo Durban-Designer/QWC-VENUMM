cc90d42d1ebc74119f5b6aa06e0a1fd12004e66b
Durban-Designer <royce.birnbaum@gmail.com>
2017-11-29 14:41:00 -0700

    removed About

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="main">
-    <div>Hello!!</div>
+    <h1>QWC-VENUMM (Quick Venom)</h1>
+    <p>The Quick Venom stack refers to an application architecture based around nine core technologies;</p>
+    <ul>
+      <li>Quasar</li>
+      <li>Webpack</li>
+      <li>Cordova</li>
+      <li>Vue.js</li>
+      <li>Express</li>
+      <li>Node.js</li>
+      <li>Ubuntu Server</li>
+      <li>MongoDb</li>
+      <li>Mongoose</li>
+    </ul>
   </div>
 </template>
 
