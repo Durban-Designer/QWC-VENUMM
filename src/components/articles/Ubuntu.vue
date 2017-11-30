87c6debe71f0e5e2e318369f3fa67d092b70cd25
Durban-Designer <royce.birnbaum@gmail.com>
2017-11-30 10:09:03 -0700

    worked on ubuntu article

--- src/components/articles/Ubuntu.vue
+++ src/components/articles/Ubuntu.vue
@@ -1,6 +1,57 @@
 <template>
   <div class="main">
-    ubuntu
+    <h1>Ubuntu Server</h1><hr/>
+    <p>
+      This article is a tutorial for setting up an Ubuntu Server instance on Amazon Web Services,
+      this server will serve to host both the API server backend as well as your actual Quick Venom Application.
+      If you are interested in setting up your own local Ubuntu Server on a Desktop computer
+      <span class="link" v-on:click="ubuntuHandsNav">click here.</span>
+    </p>
+    <h2>Requirements</h2><hr/>
+    <ul>
+      <li><a class="link" href="http://www.putty.org/">Putty</a> - An ssh client for accessing your server.</li>
+      <li><a class="link" href="https://winscp.net/eng/download.php">WinScp</a> - A visual file system browser for accessing your server.</li>
+      <li><a class="link" href="https://aws.amazon.com/">An Amazon Web Services Account</a></li>
+    </ul>
+    <h2>Step By Step</h2>
+    <p>
+      Log in to Amazon Web Services and from your dashboard click on the ec2 link to go to your
+      ec2 managment console. Click on the large blue launch instance button, then scroll until you
+      see Ubuntu Server. Click the large blue Select Button to the right and then if you are ok with a free instance
+      click on the 6.Configure Security Group tab on the upper right. Click Add Rule on the bottom left and in
+      the left dropdown box in the type column select http, then set the source column box for both the ssh and
+      http rules to anywhere. After that click Review and Launch to the bottom right and then Launch.
+      It should bring up a dialogue box asking you to create an RSA key pair.
+      (If you dont know what RSA keys are <span class="link" v-on:click="rsaNav">click here</span> for a tutorial.)
+      click the top dropdown and select create a new key pair, then type in a name in the input box below. After that
+      click the Download Key Pair button and save the key in a place you wont lose it, you'll need it to log in.
+      Now open PuttyGen which should have installed with Putty, and click the conversions tab in the upper right.
+      Click the import key option and then navigate to where you saved the .pem file and open it in PuttyGen.
+      Then click the save private key button to the bottom right, this will save the key in a .ppk format so you can
+      ssh into your server with putty. Now open back up your AWS and click the big blue Launch Instances button
+      in the lower right corner to send your server live.
+    </p><hr/>
+    <h4>Momentary break</h4><hr/>
+    <p>
+      Ok so we have spun up our amazon web server, and downloaded our RSA keys to access her. (yes servers are a she.)
+      Now we can log in with Putty and configure all of our Quick Venom dependencies. So before we get into launching
+      your application you need to have one built. So if you still haven't gone through our basics tutorial
+      <span class="link" v-on:click="basicsNav">click here.</span>
+    </p>
+    <h4>Back on Track</h4><hr/>
+    <p>
+      In your Amazon Web Services window click on the view instances button in your EC2 managment console. Copy your Public DNS (IPv4)
+      for your instance and then open up putty. In host type ubuntu@ then paste in the public DNS it should looks something like;<br/>
+      ubuntu@ec2-53-220-177-66.us-west-2.compute.amazonaws.com<br/>
+      in the saved sessions box type Amazon, then on the left click the little plus to the left of SSH and then click on auth.
+      in the bottom right click browse and navigate to your private key .ppk file we converted earlier in PuttyGen.
+      then click session on the left and click save on the lower right to save this configuration for later.
+      Now click open in the bottom right, if all goes well a dialogue box should pop up saying "server key is not cached" click yes.
+      Then it should connect and show;<br/> ubuntu@ip-111-111-111-111:~$<br/> (with different numbers for the ip.)<br/>
+      Congratulations you have succesfully connected into your Amazon Web Services instance, read on for how to configure it to run
+      Quick Venom Applications.
+    </p>
+
   </div>
 </template>
 
@@ -10,9 +61,23 @@
     data () {
       return {
       }
+    },
+    methods: {
+      ubuntuHandsNav: function () {
+        this.$router.push('/ubuntuhands')
+      },
+      rsaNav: function () {
+        this.$router.push('/rsa')
+      },
+      basicsNav: function () {
+        this.$router.push('/basics')
+      }
     }
   }
 </script>
 
 <style lang="less">
+  .link {
+    text-decoration: underline;
+  }
 </style>
