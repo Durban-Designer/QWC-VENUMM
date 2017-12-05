329cf3a72ff43bcb5756c6b6fa9ae04b53d2a250
Durban-Designer <royce.birnbaum@gmail.com>
2017-12-05 10:29:32 -0700

    Added Articles

--- src/components/articles/Vue.vue
+++ src/components/articles/Vue.vue
@@ -2,16 +2,16 @@
   <div class="main">
     <h2>Vue.js</h2>
     <p>
-      Hello and welcome to the Quick Venom Vue.js tutorial, we are going to be focusing on Vue
-      applications in Quick Venom Applications rather then explaining how to use the framework from
+      Hello and welcome to the Quick Venom Vue.js tutorial, we are going to be focusing on how Vue is used in
+      Quick Venom Applications rather then explaining how to use the framework from
       the ground up. If you need a primer on using vue <a class="link" href="https://vuejs.org/v2/guide/">
       click here</a> for the official getting started guide, its written very well.
       Now that thats cleared up lets talk about where Vue fits in our Quick Venom Application;
-      serving as our Front End Framework Vue serves to componetize our application and handle all
+      serving as our Front End Framework Vue componetizes our application and handles all
       the DOM manipulation heavy lifting for templating <span title="Single Page Applications">SPA's.</span>
       To do this all of our front end code will be contained in .vue files and be dynamically inserted into the
-      base index.html, you use Vue Router to handle all the page routing on the front end in an index.js file in
-      the router folder.
+      base index.html, you then use Vue Router to handle all the page routing on the front end in an router.js
+      file.
     </p>
     <h3>What does this mean?</h3>
     <p>
@@ -19,7 +19,65 @@
       will be explored further in later tutorials. For now lets focus in on setting up a new .vue file in your quasar
       default template. Navigate to your projects src/components directory and then make a new file called
       firstComponent.vue, then open it in Atom text editor. (As a note I'd highly recommend installing the
-      language-vue plugin for atom to get the code syntax highlighting.) In Atom 
+      language-vue plugin for atom to get the code syntax highlighting.) In Atom your .vue component consists
+      of a template, a style, and a script tag. Each of these holds all the HTML, CSS, and Javascript
+      necessary to run that component. As you can imagine this makes debuging a breeze as everything is naturally
+      self contained. As well within these .vue files you can take care of Vue directives to conditionally render classes,
+      bind rendering to data truthiness, and loads of other goodness. I'd highly reccomend toying around with
+      the getting started guide <a class="link" href="https://vuejs.org/v2/guide/">here</a> if you haven't to learn more
+      about all the features that this wonderful framework has to offer. Anyways back to Atom a basic
+      component will look like so;
+      <textarea class="code">
+        <template>
+          <div class="main">
+            Hello World!
+          </div>
+        </template>
+        <script>
+          export default {
+            name: 'firstComponent',
+            data: function () {
+              return {
+              }
+            }
+          }
+        </script>
+        <style scoped lang="less">
+          .main {
+          }
+        </style>
+      </textarea>
+      You then open router.js in the src folder of your project in Atom to add the new component to the front
+      end routing. Below the path to the / endpoint add a route for your new component. For my component above
+      it would look something like this;
+      <textarea class="code">
+        import Vue from 'vue'
+        import VueRouter from 'vue-router'
+
+        Vue.use(VueRouter)
+
+        function load (component) {
+          // '@' is aliased to src/components
+          return () => import(`@/${component}.vue`)
+        }
+
+        export default new VueRouter({
+          mode: 'hash',
+          scrollBehavior: () => ({ y: 0 }),
+
+          routes: [
+            { path: '/', component: load('Hello') },
+            { path: '/firstComponent', component: load('firstComponent') },
+
+            // Always leave this last one
+            { path: '*', component: load('Error404') } // Not found
+          ]
+        })
+      </textarea>
+      After you've edited everything save it and quasars hot reload should register the change, try navigating
+      manually to /firstComponent and if all went well you should see Hello World! I hope this tutorial shed some
+      light on vue.js and its place in the Quick Venom Stack, look forward to future articles going more in depth
+      on some of vue's best features.
     </p>
   </div>
 </template>
@@ -35,4 +93,8 @@
 </script>
 
 <style scoped lang="less">
+  .code {
+    width: 100%;
+    height: 240px;
+  }
 </style>
--- src/components/articles/Webpack.vue
+++ src/components/articles/Webpack.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="main">
-    webpack
+    <h1>Webpack</h1>
+    <p>
+      Webpack is a asset bundler and code minifier that greatly optimizes web applications,
+      it does this by a number of means. Firstly it gathers all javascript for your application
+      and puts it all together into bundles that the browser can easily download and process.
+      Secondarily it packages all your Assets such as images, fonts, and stylesheets into one
+      static location and loads them. This second function is what tends to cause the most problems
+      as Quasar auto configures it to run properly when you run quasar init default, However it fails
+      to add in any images that arent in jpeg, png, gif, or svg. So lets say you want to use webp's or
+      ico's it would't work, throwing a unrecognized character error that is really scary but actually
+      quite easy to fix. If you open your webpack.base.conf.js in your projects build folder and go to
+      line 70 which looks something like this;<br/>
+      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/<br/>
+      and then after svg simply add a line and the format you'd like to add, for example webp;<br/>
+      test: /\.(png|jpe?g|gif|svg|webp)(\?.*)?$/<br/>
+      then webpack should be able to read any images in the .webp format, and this process can be repeated
+      for any other image fonts you may need to load.
+    </p>
+    <h3>So When do I use it</h3>
+    <p>
+      Well since it's auto configured you luckily almost never have to mess with webpack again, which is a testament
+      to how stable it is. The unsung hero Webpack brings your Quick Venom Application to life, unifying your code
+      and assets thanklessly. I may have future tutorials incase I discover something additional about webpack, otherwise I hope
+      you enjoyed this brief intro to Webpack, <a class="link" href="https://webpack.js.org/concepts/"> Click Here</a> for a link
+      to the documentation if you are feeling up for a deep dive.
+    </p>
   </div>
 </template>
 
