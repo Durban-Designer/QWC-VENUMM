1679814f08d0721304be6117835b25383ab22760
Spiralis-Solutions <krisstjohn96@gmail.com>
2017-12-07 11:42:20 -0700

    here

--- src/components/articles/Quasar.vue
+++ src/components/articles/Quasar.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="main">
     <h2>Quasar</h2>
-    <p>Quasar is such a simple but vital role in the QWC-VENUMM Webstack. It's fairly easy to install basically works all by itself beautifully. It sets up dev servers with hot reload, builds for production and tons of other great development tools and code minifying. For the exact process of how it is used in conjuncture with our other technologies <span class="link" v-on:click="vueNav">click here.</span></br>
+    <p>Quasar is such a simple but vital role in the QWC-VENUMM Webstack. Build responsive websites, PWAs, hybrid mobile Apps and Electron apps, using same codebase, powered with Vue.
+      It's fairly easy to install basically works all by itself beautifully. It sets up dev servers with hot reload, builds for production and tons of other great development tools and code minifying. For the exact process of how it is used in conjuncture with our other technologies <span class="link" v-on:click="vueNav">click here.</span></br>
     So this article will be more of a resource page for more information on Quasar. Obviously one of the best and most clear resources will be <a href="http://quasar-framework.org/guide/" class="link">quasar-framework.org</a></br>
-   For updates and to show support for The fabulous creator of Quasar Framework; Razvan Stoenescu, you can visit the Quasar Patreon <a href="https://www.patreon.com/quasarframework" class="link">Here</a>.</p>
+   For updates and to show support for The fabulous creator of Quasar Framework; Razvan Stoenescu, you can visit the Quasar Patreon <a href="https://www.patreon.com/quasarframework" class="link">Here</a></br>
+ Quasar is loaded with helpful components out of the box. For one, they offer a wide variety of styling options and css assistance, allowing for some much cleaner and more efficient code. This also true for just about every aspect of app development. Layout, Navigation, Form inputs, animation, you name it, Quasar makes it better. Quasar is an indespensable tool for app development and responsive design in general and it will only stand to improve from here.
+ For an in depth look at all the different components you now have at your fingertips <a href="http://quasar-framework.org/components/" class="link">click here</a></p>
     <div class="articlaClose">
       <div class="krisImage"></div>
       <p>
@@ -89,5 +92,12 @@ h2 {
     color: #1bad4a;
     text-decoration: none;
     font-weight: bold;
+  }
+  .link:hover {
+    color: #3bdb6d;
+  }
+  p {
+    text-align: left;
+    margin-left: 10px;
   }
 </style>
